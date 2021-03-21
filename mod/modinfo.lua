95e6755cab8a1c663150008ff38cf254f465281b
Joe Wilm <joe@jwilm.com>
2021-03-21 11:31:32 -0700

    Initial Commit
    - Assets are borrowed from Place Statues mod.
    - Core drop logic borrowed from Advanced Controls
    - Alignment logic is net new
    - Workshop vdf files don't seem to support relative paths so they are
      absolute :(.

---
+++ mod/modinfo.lua
@@ -0,0 +1,39 @@
+-- repository = https://github.com/jwilm/dst-geometric-drop
+name = "Geometric Drop"
+description = "Drop items aligned to the grid"
+author = "Chaosmonkey"
+version = "0.1"
+api_version_dst = 10
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
+
+dst_compatible = true
+all_clients_require_mod = false
+client_only_mod = true
+
+folder_name = folder_name or "geometric_drop"
+if not folder_name:find("workshop-") then
+    name = name.." [dev]"
+end
+
+local string = ""
+local keys = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","F1","F2","F3","F4","F5","F6","F7","F8","F9","F10","F11","F12","LAlt","RAlt","LCtrl","RCtrl","LShift","RShift","Tab","Capslock","Space","Minus","Equals","Backspace","Insert","Home","Delete","End","Pageup","Pagedown","Print","Scrollock","Pause","Period","Slash","Semicolon","LeftBracket","RightBracket","Backslash","Up","Down","Left","Right"}
+local keylist = {}
+for i = 1, #keys do
+    keylist[i] = {description = keys[i], data = "KEY_"..string.upper(keys[i])}
+end
+keylist[#keylist + 1] = {description = "Disabled", data = false}
+
+local function AddConfig(label, name, options, default, hover)
+    return {label = label, name = name, options = options, default = default, hover = hover or ""}
+end
+
+-- Option 1. [unimplemented] Toggle visual drop indicator
+-- Option 2. Toggle grid spacing (1 tile (4 units), half tile, quarter tile)
+-- Option 3. [unimplemented] Default grid spacing and offset
+-- Option 4. Grid Spacing Offset (will optionally add 0.5 to the position)
+configuration_options = {
+    AddConfig("Toggle alignment offset", "CYCLE_OFFSET_KEY", keylist, "KEY_T", "Toggle between aligning on (eg. tile) centers or corners"),
+    AddConfig("Change resolution", "CYCLE_RESOLUTION_KEY", keylist, "KEY_G", "Cycle between 1/5, 1/4, 1/2, and full tile spacing"),
+}
---
+++ mod/modmain.lua
@@ -0,0 +1,255 @@
+PrefabFiles = {
+    "CirclePlacer",
+}
+
+Assets = {
+    Asset("ANIM", "anim/circleplacer.zip")
+}
+
+-- -----------------------
+-- Constants
+-- -----------------------
+local CIRCLE_PLACER = "circleplacer"
+
+-- Drop resolution can be  1/8, 1/4, 1/2, or 1 tile (units are in 1/4 tile)
+local DROP_RESOLUTION = {
+    [0] = 0.50,
+    [1] = 0.80,
+    [2] = 1.0,
+    [3] = 2.0,
+    [4] = 4.0
+}
+local DROP_RESOLUTION_LENGTH = 5
+
+-- Drop resolution selectors
+local DROP_RESOLUTION_FIFTH = 0
+local DROP_RESOLUTION_QUARTER = 1
+local DROP_RESOLUTION_HALF = 2
+local DROP_RESOLUTION_TILE = 3
+
+-- The offsets are indexed by whether they are enabled. Offsets represent 1/2
+-- of the drop resolution for each step.
+local DROP_OFFSETS =  {
+    [0] = { [0] = 0.0,  [1] = 0.0, [2] = 0.0, [3] = 0.0, [4] = 0.0},
+    [1] = { [0] = 0.25, [1] = 0.4, [2] = 0.5, [3] = 1.0, [4] = 2.0 },
+}
+local DROP_OFFSETS_LENGTH = 2
+
+-- Drop offset selectors
+local DROP_OFFSET_DISABLED = 0
+local DROP_OFFSET_ENABLED  = 1
+
+-- -----------------------
+-- Cached Globals
+-- -----------------------
+local SpawnPrefab = GLOBAL.SpawnPrefab
+local TheInput = GLOBAL.TheInput
+local BufferedAction = GLOBAL.BufferedAction
+local SendRPCToServer = GLOBAL.SendRPCToServer
+local ACTIONS = GLOBAL.ACTIONS
+local CONTROL_FORCE_STACK = GLOBAL.CONTROL_FORCE_STACK
+local CONTROL_FORCE_TRADE = GLOBAL.CONTROL_FORCE_TRADE
+local CONTROL_SECONDARY = GLOBAL.CONTROL_SECONDARY
+local RPC = GLOBAL.RPC
+local ThePlayer
+local TheWorld
+
+-- -----------------------
+-- Variables
+-- -----------------------
+local placersVisible = false
+local dropResolution = DROP_RESOLUTION_HALF
+local dropOffset = DROP_OFFSET_ENABLED
+
+-- -----------------------
+-- Implementation
+-- -----------------------
+local function GetKeyConfig(config)
+    local key = GetModConfigData(config, true)
+    if type(key) == "string" and GLOBAL:rawget(key) then
+        key = GLOBAL[key]
+    end
+    return type(key) == "number" and key or -1
+end
+
+local CYCLE_OFFSET_KEY = GetKeyConfig("CYCLE_OFFSET_KEY", "KEY_LEFTBRACKET")
+local CYCLE_RESOLUTION_KEY = GetKeyConfig("CYCLE_RESOLUTION_KEY", "KEY_RIGHTBRACKET")
+
+local function round(num)
+    return math.floor(num + 0.5)
+end
+
+local function NewCirclePlacer(anim, scale)
+    local placer = SpawnPrefab(CIRCLE_PLACER)
+    placer.AnimState:PlayAnimation(anim, true)
+    placer.Transform:SetScale(scale, scale, scale)
+    placer:Hide()
+    return placer
+end
+
+local CenterDropPlacer
+local AdjacentDropPlacer
+
+local function TogglePlacers(method)
+    CenterDropPlacer[method](CenterDropPlacer)
+    for i=0,7 do
+        local placer = AdjacentDropPlacer[i]
+        placer[method](placer)
+    end
+end
+
+local function InitializePlacers()
+    CenterDropPlacer = NewCirclePlacer("on", 0.6)
+    AdjacentDropPlacer = {
+        [0] = NewCirclePlacer("off", 0.6),
+        [1] = NewCirclePlacer("off", 0.6),
+        [2] = NewCirclePlacer("off", 0.6),
+        [3] = NewCirclePlacer("off", 0.6),
+        [4] = NewCirclePlacer("off", 0.6),
+        [5] = NewCirclePlacer("off", 0.6),
+        [6] = NewCirclePlacer("off", 0.6),
+        [7] = NewCirclePlacer("off", 0.6),
+    }
+end
+
+local function AlignToGrid(pos)
+	local x = pos.x
+	local z = pos.z
+	
+    -- Lookup correct resolution and offset
+    local resolution = DROP_RESOLUTION[dropResolution]
+    local offset  = DROP_OFFSETS[dropOffset][dropResolution]
+
+    -- Adjust coordinates based on resolution and offset
+    pos.x = resolution * round((x + offset) / resolution) - offset
+    pos.z = resolution * round((z + offset) / resolution) - offset
+
+    return pos
+end
+
+local function UpdatePlacers()
+    if not placersVisible then return end
+    local center = AlignToGrid(TheInput:GetWorldPosition())
+
+    local x = center.x
+    local z = center.z
+	
+    local resolution = DROP_RESOLUTION[dropResolution]
+
+    AdjacentDropPlacer[0].Transform:SetPosition(x - resolution, -0.1, z - resolution)
+    AdjacentDropPlacer[1].Transform:SetPosition(x - resolution, -0.1, z             )
+    AdjacentDropPlacer[2].Transform:SetPosition(x - resolution, -0.1, z + resolution)
+    AdjacentDropPlacer[3].Transform:SetPosition(x             , -0.1, z - resolution)
+    AdjacentDropPlacer[4].Transform:SetPosition(x             , -0.1, z + resolution)
+    AdjacentDropPlacer[5].Transform:SetPosition(x + resolution, -0.1, z - resolution)
+    AdjacentDropPlacer[6].Transform:SetPosition(x + resolution, -0.1, z             )
+    AdjacentDropPlacer[7].Transform:SetPosition(x + resolution, -0.1, z + resolution)
+    CenterDropPlacer.Transform:SetPosition(x, -0.1, z)
+end
+
+TheInput:AddKeyUpHandler(CYCLE_OFFSET_KEY, function ()
+    dropOffset = math.fmod(dropOffset + 1, DROP_OFFSETS_LENGTH)
+end)
+
+TheInput:AddKeyUpHandler(CYCLE_RESOLUTION_KEY, function ()
+    dropResolution = math.fmod(dropResolution + 1, DROP_RESOLUTION_LENGTH)
+end)
+
+local function DropActiveItemOnGrid(pos, active_item)
+    local playercontroller = ThePlayer.components.playercontroller
+
+    pos = AlignToGrid(pos)
+
+    local act = BufferedAction(ThePlayer, nil, ACTIONS.DROP, active_item, pos)
+    if playercontroller.locomotor then
+        act.options.wholestack = not TheInput:IsControlPressed(CONTROL_FORCE_STACK)
+        act.preview_cb = function()
+            SendRPCToServer(RPC.LeftClick, ACTIONS.DROP.code, pos.x, pos.z, nil, true, playercontroller:EncodeControlMods(), nil, act.action.mod_name)
+        end
+        playercontroller:DoAction(act)
+    else
+        SendRPCToServer(RPC.LeftClick, ACTIONS.DROP.code, pos.x, pos.z, nil, true, playercontroller:EncodeControlMods(), act.action.canforce, act.action.mod_name)
+    end
+end
+
+AddComponentPostInit("playercontroller", function(self)
+    ThePlayer = GLOBAL.ThePlayer
+    TheWorld = GLOBAL.TheWorld
+    InitializePlacers()
+
+    local active_item
+
+    local function DoModifiedLeftClickAction(act)
+        if act.action == ACTIONS.DROP then
+            DropActiveItemOnGrid(TheInput:GetWorldPosition(), active_item)
+            return true
+        end
+        return false
+    end
+
+    local PlayerControllerOnLeftClick = self.OnLeftClick
+    self.OnLeftClick = function(self, down)
+        if not down or TheInput:GetHUDEntityUnderMouse() or self:IsAOETargeting() or self.placer_recipe then
+            return PlayerControllerOnLeftClick(self, down)
+        end
+        local act = self:GetLeftMouseAction()
+        if act then
+            if DoModifiedLeftClickAction(act) then return end
+        end
+        PlayerControllerOnLeftClick(self, down)
+    end
+
+    local PlayerControllerOnUpdate = self.OnUpdate
+    local function InGame()
+        return ThePlayer and ThePlayer.HUD and not ThePlayer.HUD:HasInputFocus()
+    end
+    self.OnUpdate = function(self, dt)
+        local next_active_item = ThePlayer.replica.inventory:GetActiveItem()
+        if not active_item and next_active_item then
+            placersVisible = true
+            TogglePlacers("Show")
+        elseif active_item and not next_active_item then
+            placersVisible = false
+            TogglePlacers("Hide")
+        end
+        active_item = next_active_item
+
+        if InGame() then UpdatePlacers() end
+
+        return PlayerControllerOnUpdate(self, dt)
+    end
+
+end)
+
+local function DropItemFromSlot(slot, item, single_item)
+    local inventory = ThePlayer.replica.inventory
+    local inventoryitem = item.replica.inventoryitem
+    if not single_item and not inventory:GetActiveItem()
+      and inventoryitem:CanGoInContainer() and not inventoryitem:CanOnlyGoInPocket() then
+        if slot.equipslot then
+            inventory:TakeActiveItemFromEquipSlot(slot.equipslot)
+        elseif slot.num then
+            slot.container:TakeActiveItemFromAllOfSlot(slot.num)
+        end
+        DropActiveItemOnGrid(ThePlayer:GetPosition(), item)
+    else
+        inventory:DropItemFromInvTile(item, single_item)
+    end
+end
+
+local function InvSlotPostInit(self)
+    local InvSlotOnControl = self.OnControl
+    self.OnControl = function(self, control, down)
+        if down and control == CONTROL_SECONDARY and self.tile then
+            local single_item = TheInput:IsControlPressed(CONTROL_FORCE_STACK)
+            if TheInput:IsControlPressed(CONTROL_FORCE_TRADE) or single_item then
+                DropItemFromSlot(self, self.tile.item, single_item)
+                return true
+            end
+        end
+        return InvSlotOnControl(self, control, down)
+    end
+end
+
+AddClassPostConstruct("widgets/invslot", InvSlotPostInit)
+AddClassPostConstruct("widgets/equipslot", InvSlotPostInit)
