9bba7849f028485c25e70d00665def317249091c
Joe Wilm <joe@jwilm.com>
2021-03-23 09:46:47 -0700

    bump version to 1.0

--- mod/modinfo.lua
+++ mod/modinfo.lua
@@ -2,7 +2,7 @@
 name = "Geometric Drop"
 description = "Drop items aligned to the grid with a visual placer"
 author = "Chaosmonkey"
-version = "0.4.1"
+version = "1.0"
 api_version_dst = 10
 
 icon_atlas = "modicon.xml"
